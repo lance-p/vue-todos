26058c7868a341079918a76b651b195b8b06a9b5
lance p <lance.parish@gmail.com>
2018-02-01 08:10:55 -0500

    First commit - reimplement todos demo

---
+++ src/components/Todo.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div>{{ $route.params.filter }}
+    <ul>
+      <li  v-for="todo in filteredTodos"
+  :key="todo.id">{{todo.title}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  
+  const filters = {
+      all(todos) {
+            return todos;
+        },
+      complete(todos) {
+        return todos.filter(todo => todo.complete);
+      },
+      actiive(todos)  {
+        return todos.filter(todo => !todo.complete);
+      }
+  }
+
+  export default {
+    data() {
+      return {
+        todos: [
+          {id: 1, title: 'walk dog', complete: false },
+          {id: 2, title: 'get coffee', complete: false },
+          {id: 3, title: 'work on vue', complete: true},
+        ]
+      }
+    },
+    computed: {
+      filteredTodos() {
+        const filter =  this.$route.params.filter || 'all'
+        return filters[filter](this.todos);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
